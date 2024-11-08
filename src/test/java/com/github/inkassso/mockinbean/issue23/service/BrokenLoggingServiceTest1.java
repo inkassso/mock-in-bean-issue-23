26a54225623e4074699fa413215d7c6d685fb933
Samuel Castillo <samuel.castillo@extern.brz.gv.at>
2024-11-08 12:27:30 +0100

    Add testcases where Mockito is unable to recognize its own mock

---
+++ src/test/java/com/github/inkassso/mockinbean/issue23/service/BrokenLoggingServiceTest1.java
@@ -0,0 +1,40 @@
+package com.github.inkassso.mockinbean.issue23.service;
+
+import static org.mockito.Mockito.verify;
+
+import com.teketik.test.mockinbean.SpyInBean;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * A broken test, creating the spy using {@link SpyInBean} from the service proxy and automatically replacing it
+ * in the {@link LoggingService} and the test class.
+ *
+ * Apparently, Mockito is not able to recognize its own mock within {@link org.mockito.Mockito#verify(Object)},
+ * because it doesn't unwrap the proxy when creating the mock, and thus stores information about the mock
+ * in a Map under a hashCode of the proxy.
+ * Then during verification, when looking up the mock information again, it actually does unwrap the proxy beforehand,
+ * therefore it is unable to find the mock information anymore, because the hashCode of the proxy and hashCode the actual
+ * service instance do not match.
+ *
+ * Debug {@code org.mockito.internal.creation.bytebuddy.InlineDelegateByteBuddyMockMaker#getHandler(Object)}
+ * where the lookup happens.
+ * See {@link org.springframework.boot.test.mock.mockito.SpringBootMockResolver} responsible for unwrapping the proxy.
+ */
+@SpringBootTest
+class BrokenLoggingServiceTest1 {
+
+    @Autowired
+    protected LoggingService loggingService;
+
+    @SpyInBean(LoggingService.class)
+    private ProviderService providerService;
+
+    @Test
+    void testLogCurrentValue() {
+        loggingService.logCurrentValue();
+
+        verify(providerService).provideValue();
+    }
+}
---
+++ src/test/java/com/github/inkassso/mockinbean/issue23/service/BrokenLoggingServiceTest2.java
@@ -0,0 +1,47 @@
+package com.github.inkassso.mockinbean.issue23.service;
+
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.ReflectionTestUtils;
+
+/**
+ * A broken test, manually creating the spy from the {@link ProviderService} proxy and injecting that
+ * into the {@link LoggingService}.
+ *
+ * Mockito doesn't recognize its own spy here either due to the same issue, it doesn't unwrap the proxy at mock creation,
+ * but it does at invocation verification.
+ */
+@SpringBootTest
+class BrokenLoggingServiceTest2 {
+    
+    @Autowired
+    protected LoggingService loggingService;
+    @Autowired
+    private ProviderService providerService;
+
+    private ProviderService providerServiceSpy;
+
+    @BeforeEach
+    public void setUp() {
+        providerServiceSpy = Mockito.spy(providerService);
+        ReflectionTestUtils.setField(loggingService, "providerService", providerServiceSpy);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        ReflectionTestUtils.setField(loggingService, "providerService", providerService);
+    }
+
+    @Test
+    void testLogCurrentValue() {
+        loggingService.logCurrentValue();
+
+        verify(providerServiceSpy).provideValue();
+    }
+}
