e964043ca784da3a1748067d6c8d01f15f33e37a
Samuel Castillo <samuel.castillo@extern.brz.gv.at>
2024-11-08 12:48:26 +0100

    Add another working test sample using @MockInBean

---
+++ src/test/java/com/github/inkassso/mockinbean/issue23/service/WorkingLoggingServiceTest3.java
@@ -0,0 +1,32 @@
+package com.github.inkassso.mockinbean.issue23.service;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.teketik.test.mockinbean.MockInBean;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * A working test, creating a mock using {@link MockInBean} from the service proxy and automatically replacing it
+ * in the {@link LoggingService} and the test class.
+ */
+@SpringBootTest
+class WorkingLoggingServiceTest3 {
+
+    @Autowired
+    protected LoggingService loggingService;
+
+    @MockInBean(LoggingService.class)
+    private ProviderService providerService;
+
+    @Test
+    void testLogCurrentValue() {
+        when(providerService.provideValue()).thenReturn("mocked value");
+
+        loggingService.logCurrentValue();
+
+        verify(providerService).provideValue();
+    }
+}
